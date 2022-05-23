c9db979430b79486bee419f5d6400cf3b1bc431b
HizbullahBhatti <hizbullah.kcsf18@iba-suk.edu.pk>
2022-05-23 13:52:07 +0500

    Connection with MongoDb and collection created

--- api/routes/student.js
+++ api/routes/student.js
@@ -1,6 +1,10 @@
 const express = require('express');
-
 const router = express.Router();
+const Student = require('../model/student');
+const mongoose = require('mongoose')
+
+
+
 
 router.get('/',(req,res,next)=>{
     res.status(200).json({
@@ -10,8 +14,26 @@ router.get('/',(req,res,next)=>{
 })
 
 router.post('/',(req,res,next)=>{
-    res.status(200).json({
-        msg:"This is Student post Request"
+    const student = new Student({
+       _id:mongoose.Types.ObjectId,
+        name:req.body.name,
+        email:req.body.email,
+        phone:req.body.phone,
+        gender:req.body.gender
+
+    })
+    student.save()
+    .then(result=>{
+        console.log(result);
+        res.status(200).json({
+            newStudent:result
+        })
+    })
+    .catch(err=>{
+        console.log(err);
+        res.status(500).json({
+            error:err
+        })
     })
 
 })
--- app.js
+++ app.js
@@ -3,9 +3,30 @@ const express = require('express');
 const app = express.Router();
 const studentRoute = require('./api/routes/student')
 const facultyRoute = require('./api/routes/faculty')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+
+mongoose.connect('mongodb+srv://student:student@cluster0.ykaes.mongodb.net/?retryWrites=true&w=majority')
+
+mongoose.connection.on('error',err=>{
+    console.log('Database not Connected')
+});
+
+mongoose.connection.on('connected',connected=>{
+    console.log('Connected with Database Successfully')
+})
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
 
 app.use('/student',studentRoute)
 app.use('/faculty',facultyRoute)
+
+app.use((req,res,next)=>{
+    res.status(404).json({
+        error:"Bad URL Request"
+    })
+})
 
 app.use((req,res,next)=>{
     res.status(200).json({
